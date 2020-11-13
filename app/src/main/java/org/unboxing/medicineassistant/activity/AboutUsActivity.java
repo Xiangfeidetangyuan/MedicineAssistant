cf8a06b18170a4b3802abfc2a0b787438062f662
mxli <limingxincube@outlook.com>
2020-11-13 10:12:40 +0800

    问题反馈界面及alert弹窗，没有实现发邮件

--- app/src/main/java/org/unboxing/medicineassistant/activity/AboutUsActivity.java
+++ app/src/main/java/org/unboxing/medicineassistant/activity/AboutUsActivity.java
@@ -1,8 +1,10 @@
 package org.unboxing.medicineassistant.activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import org.unboxing.medicineassistant.R;
 
@@ -13,4 +15,18 @@ public class AboutUsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
     }
+
+    public void sendFeedbackMessage(View view){
+        //获取信息发送邮件
+        show();
+    }
+
+    private void show(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this).setIcon(R.mipmap.icon).setTitle("反馈成功")
+                .setMessage("感谢您的反馈，我们的工作人员会与您联系！").
+                        setPositiveButton("知道了", null);
+        builder.show();
+    }
+
+
 }
